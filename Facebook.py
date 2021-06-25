362baf7145fea966faca8aede5df054f45c8df88
DEVL_NOOB <69730462+yunanaexploit@users.noreply.github.com>
2021-06-25 21:07:19 +0800

    Update Facebook.py

--- Facebook.py
+++ Facebook.py
@@ -1,8 +1,9 @@
-import os
-import sys
-import requests
-
-def crack() :
- os.system('clear') 
- url="https://mbasic.facebook.com/login.php"
- 
+i=1
+cs=11111111111
+xd=1000
+f=open('id.txt')
+while i < 9999:
+ print xd+cs
+ f.write(xd+cs)
+ cs+=1
+ f.close()
